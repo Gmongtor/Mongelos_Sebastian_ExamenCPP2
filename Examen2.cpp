8f42e4d05578bf906ff515706ea93ad3171c9919
Gmongtor <gmongtor@myuax.com>
2023-12-14 12:11:43 +0100

    Materias

--- Examen2.cpp
+++ Examen2.cpp
@@ -5,42 +5,49 @@
 using namespace std;
 
 struct Estudiante {
-    std::string nombre;
+    string nombre;
     int edad;
     float promedio;
     vector<string> materias;
 };
- void imprimirEstudiante(Estudiante estudiante) {
-     cout << "Nombre: " << estudiante.nombre << endl;
-     cout << "Edad: " << estudiante.edad << endl;
-     cout << "Promedio: " << estudiante.promedio << endl;
-     cout << "Materias: " << endl;
-     for (int i = 0; i < estudiante.materias.size(); i++) {
-         cout << estudiante.materias[i] << endl;
-     }
-     void agregarMateria(Estudiante estudiante, string materia) {
-         estudiante.materias.push_back(materia);
-     }
-     void eliminarMateria(Estudiante estudiante, string materia) {
-         for (int i = 0; i < estudiante.materias.size(); i++) {
-             if (estudiante.materias[i] == materia) {
-                 estudiante.materias.erase(estudiante.materias.begin() + i);
-             }
-         }
-     }
- }
-    int main() {
-        Estudiante estudiante1;
-        estudiante1.nombre = "Juan";
-        estudiante1.edad = 20;
-        estudiante1.promedio = 9.5;
-        estudiante1.materias.push_back("Matematicas");
-        estudiante1.materias.push_back("Fisica");
-        estudiante1.materias.push_back("Quimica");
-        imprimirEstudiante(estudiante1);
-        agregarMateria(estudiante1, "Programacion");
-        imprimirEstudiante(estudiante1);
-        eliminarMateria(estudiante1, "Fisica");
-        imprimirEstudiante(estudiante1);
-        return 0;
+
+void imprimirEstudiante(Estudiante estudiante) {
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+    cout << "Materias: " << endl;
+    for (int i = 0; i < estudiante.materias.size(); i++) {
+        cout << estudiante.materias[i] << endl;
     }
+}
+
+void agregarMateria(Estudiante& estudiante, string materia) {
+    estudiante.materias.push_back(materia);
+}
+
+void eliminarMateria(Estudiante& estudiante, string materia) {
+    for (int i = 0; i < estudiante.materias.size(); i++) {
+        if (estudiante.materias[i] == materia) {
+            estudiante.materias.erase(estudiante.materias.begin() + i);
+            break;
+        }
+    }
+}
+
+int main() {
+    Estudiante estudiante1;
+    estudiante1.nombre = "Sebastian";
+    estudiante1.edad = 18;
+    estudiante1.promedio = 4.5;
+    estudiante1.materias.push_back("Matematicas");
+    estudiante1.materias.push_back("Fisica");
+    estudiante1.materias.push_back("Quimica");
+
+    imprimirEstudiante(estudiante1);
+    agregarMateria(estudiante1, "Programacion");
+    imprimirEstudiante(estudiante1);
+    eliminarMateria(estudiante1, "Fisica");
+    imprimirEstudiante(estudiante1);
+
+    return 0;
+}
