8a308b3daa2d2ccdb392805f9d1d4326fddbe6f7
Gmongtor <gmongtor@myuax.com>
2023-12-14 12:49:47 +0100

    C

--- C.cpp
+++ C.cpp
@@ -19,3 +19,9 @@ typedef struct {
     Asistencia asistencias[50];
     int numAsistencias;
 } Estudiante;
+
+void agregarMateria(Estudiante* est, const char* materia) {
+    if (est->numMaterias < 10) {
+        strcpy(est->materias[est->numMaterias++], materia);
+    }
+}
