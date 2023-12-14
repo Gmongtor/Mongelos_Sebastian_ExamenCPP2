93a5a732b7900a52b69ca9ab41fe4a0865bb22c3
Gmongtor <gmongtor@myuax.com>
2023-12-14 12:48:39 +0100

    C

--- C.cpp
+++ C.cpp
@@ -9,3 +9,13 @@ typedef struct {
     char materia[50];
     EstadoAsistencia estado;
 } Asistencia;
+
+typedef struct {
+    char nombre[50];
+    int edad;
+    float promedio;
+    char materias[10][50]; // Suponiendo un máximo de 10 materias
+    int numMaterias;
+    Asistencia asistencias[50]; // Suponiendo un máximo de 50 asistencias
+    int numAsistencias;
+} Estudiante;
