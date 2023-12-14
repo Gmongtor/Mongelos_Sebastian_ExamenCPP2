8b41ff48d8e92872e22567648ea193736f24f51f
Gmongtor <gmongtor@myuax.com>
2023-12-14 12:48:22 +0100

    C

--- C.cpp
+++ C.cpp
@@ -1,3 +1,11 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
+typedef enum { Asistio, Falta, Tardanza } EstadoAsistencia;
+
+typedef struct {
+    char fecha[11];
+    char materia[50];
+    EstadoAsistencia estado;
+} Asistencia;
