13609493aa32a069c29956a6be2a60389821f365
Gmongtor <gmongtor@myuax.com>
2023-12-14 12:22:26 +0100

    Asistencia

--- Examen2.cpp
+++ Examen2.cpp
@@ -2,6 +2,7 @@
 #include <string>
 #include <vector>
 #include <map>
+
 using namespace std;
 
 enum EstadoAsistencia { Asistio, Falta, Tardanza };
@@ -10,7 +11,6 @@ struct Asistencia {
     string fecha;
     string materia;
     EstadoAsistencia estado;
-    vector<Asistencia> asistencias;
 };
 
 struct Estudiante {
@@ -18,41 +18,35 @@ struct Estudiante {
     int edad;
     float promedio;
     vector<string> materias;
+    vector<Asistencia> asistencias;
+
+    void agregarMateria(const string& materia) {
+        materias.push_back(materia);
+    }
+
+    void registrarAsistencia(const string& fecha, const string& materia, EstadoAsistencia estado) {
+        Asistencia nuevaAsistencia = {fecha, materia, estado};
+        asistencias.push_back(nuevaAsistencia);
+    }
+
+    void mostrarAsistencias() const {
+        cout << "Asistencias de " << nombre << ":" << endl;
+        for (const auto& asistencia : asistencias) {
+            cout << "Fecha: " << asistencia.fecha << ", Materia: " << asistencia.materia
+                 << ", Estado: " << (asistencia.estado == Asistio ? "Asistió" : asistencia.estado == Falta ? "Falta" : "Tardanza") << endl;
+        }
+    }
 };
 
-void imprimirEstudiante(Estudiante estudiante) {
+void imprimirEstudiante(const Estudiante& estudiante) {
     cout << "Nombre: " << estudiante.nombre << endl;
     cout << "Edad: " << estudiante.edad << endl;
     cout << "Promedio: " << estudiante.promedio << endl;
     cout << "Materias: " << endl;
-    for (int i = 0; i < estudiante.materias.size(); i++) {
-        cout << estudiante.materias[i] << endl;
+    for (const auto& materia : estudiante.materias) {
+        cout << materia << endl;
     }
     estudiante.mostrarAsistencias();
-}
-void registrarAsistencia(const string& fecha, const string& materia, EstadoAsistencia estado) {
-    Asistencia nuevaAsistencia = {fecha, materia, estado};
-    asistencias.push_back(nuevaAsistencia);
-}
-void mostrarAsistencias() const {
-    cout << "Asistencias de " << nombre << ":" << endl;
-    for (const auto& asistencia : asistencias) {
-        cout << "Fecha: " << asistencia.fecha << ", Materia: " << asistencia.materia
-             << ", Estado: " << (asistencia.estado == Asistio ? "Asistió" : asistencia.estado == Falta ? "Falta" : "Tardanza") << endl;
-    }
-}
-
-void agregarMateria(Estudiante& estudiante, string materia) {
-    estudiante.materias.push_back(materia);
-}
-
-void eliminarMateria(Estudiante& estudiante, string materia) {
-    for (int i = 0; i < estudiante.materias.size(); i++) {
-        if (estudiante.materias[i] == materia) {
-            estudiante.materias.erase(estudiante.materias.begin() + i);
-            break;
-        }
-    }
 }
 
 int main() {
@@ -60,15 +54,17 @@ int main() {
     estudiante1.nombre = "Sebastian";
     estudiante1.edad = 18;
     estudiante1.promedio = 4.5;
-    estudiante1.materias.push_back("Matematicas");
-    estudiante1.materias.push_back("Fisica");
-    estudiante1.materias.push_back("Quimica");
+    estudiante1.agregarMateria("Matematicas");
+    estudiante1.agregarMateria("Fisica");
+    estudiante1.agregarMateria("Quimica");
 
-    imprimirEstudiante(estudiante1);
-    agregarMateria(estudiante1, "Programacion");
-    imprimirEstudiante(estudiante1);
-    eliminarMateria(estudiante1, "Fisica");
+    estudiante1.registrarAsistencia("2023-12-10", "Matematicas", Asistio);
+    estudiante1.registrarAsistencia("2023-12-11", "Fisica", Falta);
+    estudiante1.registrarAsistencia("2023-12-12", "Quimica", Tardanza);
+
     imprimirEstudiante(estudiante1);
 
     return 0;
 }
+
+
