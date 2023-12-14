acd7493e3751e65d555482dede00e82e2dde5a18
Gmongtor <gmongtor@myuax.com>
2023-12-14 12:08:10 +0100

    materias

--- Examen2.cpp
+++ Examen2.cpp
@@ -10,19 +10,23 @@ struct Estudiante {
     float promedio;
     vector<string> materias;
 };
- void imprimirEstudiante(Estudiante estudiante){
+ void imprimirEstudiante(Estudiante estudiante) {
      cout << "Nombre: " << estudiante.nombre << endl;
      cout << "Edad: " << estudiante.edad << endl;
      cout << "Promedio: " << estudiante.promedio << endl;
+     cout << "Materias: " << endl;
+     for (int i = 0; i < estudiante.materias.size(); i++) {
+         cout << estudiante.materias[i] << endl;
+     }
+     void agregarMateria(Estudiante estudiante, string materia) {
+         estudiante.materias.push_back(materia);
+     }
+     void eliminarMateria(Estudiante estudiante, string materia) {
+         for (int i = 0; i < estudiante.materias.size(); i++) {
+             if (estudiante.materias[i] == materia) {
+                 estudiante.materias.erase(estudiante.materias.begin() + i);
+             }
+         }
+     }
  }
- void agregarMateria(Estudiante estudiante, string materia){
-     estudiante.materias.push_back(materia);
- }
-int main() {
-    Estudiante estudiante1;
-    estudiante1.nombre = "Sebastian";
-    estudiante1.edad = 18;
-    estudiante1.promedio = 0;
-    imprimirEstudiante(estudiante1);
-    return 0;
-}
+
